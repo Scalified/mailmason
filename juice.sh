3f12e90038c570040f7ee8e90da7a827eda109b5
Sergey Siryk (Office) <serg.siryk@gmail.com>
2018-12-20 13:39:11 +0200

    Reset password template

--- juice.sh
+++ juice.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-npm run juice -- ./dist/welcome-sweetcv.html ./inlined-dist/welcome-sweetcv.html
+npm run juice -- ./dist/welcome_sweetcv.html ./inlined-dist/email-verification.html
+npm run juice -- ./dist/password_reset_sweetcv.html ./inlined-dist/email-password-reset.html

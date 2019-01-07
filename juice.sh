16bd35aa11a265484f14a2fe72bf296d570f4b45
Sergey Siryk <serg.siryk@gmail.com>
2019-01-07 16:49:24 +0200

    Polished templates

--- juice.sh
+++ juice.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-npm run juice -- ./dist/welcome_sweetcv.html ./inlined-dist/email-verification.html
-npm run juice -- ./dist/password_reset_sweetcv.html ./inlined-dist/email-password-reset.html
+npm run juice -- ./dist/welcome_sweetcv.html ./inlined-dist/email-verification.hbs
+npm run juice -- ./dist/password_reset_sweetcv.html ./inlined-dist/email-password-reset.hbs

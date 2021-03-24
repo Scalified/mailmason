b9847ec5c26f36848ce0c13b08930b66942bbf9d
Sergey Siryk (Office) <serg.siryk@gmail.com>
2021-03-24 16:04:45 +0200

    Add multilanguage template

--- juice.sh
+++ juice.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 npm run juice -- ./dist/welcome_sweetcv.html ./inlined-dist/email-verification.hbs
 npm run juice -- ./dist/password_reset_sweetcv.html ./inlined-dist/email-password-reset.hbs
+
+npm run juice -- ./dist/welcome_sweetcv_ru.html ./inlined-dist/email-verification_ru.hbs
+npm run juice -- ./dist/password_reset_sweetcv_ru.html ./inlined-dist/email-password-reset_ru.hbs
+
+npm run juice -- ./dist/welcome_sweetcv_ua.html ./inlined-dist/email-verification_ua.hbs
+npm run juice -- ./dist/password_reset_sweetcv_ua.html ./inlined-dist/email-password-reset_ua.hbs

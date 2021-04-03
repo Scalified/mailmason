925d8886d2a8c9d7cedb85d88f4be115568fa267
Serhii Siryk <serg.siryk@gmail.com>
2021-04-04 00:55:31 +0300

    Welcome email template + some small changes to existing templates

--- juice.sh
+++ juice.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-npm run juice -- ./dist/welcome_sweetcv.html ./inlined-dist/email-verification.hbs
+npm run juice -- ./dist/email_verification_sweetcv.html ./inlined-dist/email-verification.hbs
+npm run juice -- ./dist/welcome_sweetcv.html ./inlined-dist/welcome.hbs
 npm run juice -- ./dist/password_reset_sweetcv.html ./inlined-dist/email-password-reset.hbs
 
-npm run juice -- ./dist/welcome_sweetcv_ru.html ./inlined-dist/email-verification_ru.hbs
+npm run juice -- ./dist/email_verification_sweetcv_ru.html ./inlined-dist/email-verification_ru.hbs
+npm run juice -- ./dist/welcome_sweetcv_ru.html ./inlined-dist/welcome_ru.hbs
 npm run juice -- ./dist/password_reset_sweetcv_ru.html ./inlined-dist/email-password-reset_ru.hbs
 
-npm run juice -- ./dist/welcome_sweetcv_ua.html ./inlined-dist/email-verification_ua.hbs
+npm run juice -- ./dist/email_verification_sweetcv_ua.html ./inlined-dist/email-verification_ua.hbs
+npm run juice -- ./dist/welcome_sweetcv_ua.html ./inlined-dist/welcome_ua.hbs
 npm run juice -- ./dist/password_reset_sweetcv_ua.html ./inlined-dist/email-password-reset_ua.hbs
